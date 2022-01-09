ccf3fafd013fe13113563a78ccd21d56fad279f2
Paxagi <paxagi@gmail.com>
2022-01-09 22:59:00 +0300

    feat: add custom hook file usePost.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyModal from './components/UI/modal/MyModal';
+import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
 
 function App() {
@@ -17,18 +18,8 @@ function App() {
   // HOOK STATEMENTS
   const [filter, setFilter] = useState({sort: '', query:''})
   const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => { 
-    console.log('getSortedPost is worked')
-    if (filter.sort) {
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [sortedPosts, filter.query])
+  // CUSTOM HOOK STATEMENTS
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
